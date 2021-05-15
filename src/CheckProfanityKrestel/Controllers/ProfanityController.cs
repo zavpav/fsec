cd60442375f5c2808e43b1c761f271bbbf4eb59d
Павел Завьялов <zavjalov@gmail.com>
2021-05-15 20:40:25 +0300

    Krestel Profanity Check endpoint

--- src/CheckProfanityKrestel/Controllers/ProfanityController.cs
+++ src/CheckProfanityKrestel/Controllers/ProfanityController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using CheckProfanityKrestel.Lib;
 using Microsoft.AspNetCore.Mvc;
@@ -22,12 +23,33 @@ namespace CheckProfanityKrestel.Controllers
         }
 
         [HttpGet]
-        public CheckProfanityResult Get(Stream inputStream, EnumExecuteDetail exectionDetail)
+        public string Get()
+        {
+            return "OK";
+        }
+
+        //[HttpPost]
+        //public CheckProfanityResult Post(Stream inputStream, EnumExecuteDetail exectionDetail)
+        //{
+        //    var profanityList = new ProfanityListXmlSample();
+        //    var service = new CheckProfanityService(profanityList);
+
+        //    return service.CheckProfanity(inputStream, exectionDetail);
+        //}
+        
+        [HttpPost]
+        [Produces("application/json")]
+        public async Task<CheckProfanityResult> Post(EnumExecuteDetail exectionDetail)
         {
             var profanityList = new ProfanityListXmlSample();
             var service = new CheckProfanityService(profanityList);
+            
+            byte[] buf = new byte[(int)Request.ContentLength.Value];
+            await Request.Body.ReadAsync(buf, 0, (int)Request.ContentLength.Value);
 
-            return service.CheckProfanity(inputStream, exectionDetail);
+            var ms = new MemoryStream(buf);
+
+            return service.CheckProfanity(ms, exectionDetail);
         }
     }
 }
