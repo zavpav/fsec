9872e12684fad3dfb1ab74b6644f97d2bc253a0b
Павел Завьялов <zavjalov@gmail.com>
2021-05-16 16:51:33 +0300

    Pretesting aws lambda deploy

---
+++ src/ChechProfanity/BasketFunctions.cs
@@ -0,0 +1,27 @@
+﻿// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+
+using Amazon.Lambda.Core;
+using ProfanityList.WordList;
+
+namespace CheckProfanityAwsLambda
+{
+    public class BasketFunctions
+    {
+        private ProfanityListS3BucketService Basket { get; }
+
+        public BasketFunctions()
+        {
+            this.Basket = new ProfanityListS3BucketService();
+        }
+        
+        public BasketEditResult BasketAddWordHandler(string word, ILambdaContext context)
+        {
+            return this.Basket.Add(word);
+        }
+
+        public BasketEditResult BasketRemoveWordHandler(string word, ILambdaContext context)
+        {
+            return this.Basket.Add(word);
+        }
+    }
+}
